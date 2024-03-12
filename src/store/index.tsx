86359d58abf3dd58c354175a6744bab16fbb9777
XSpace Solution <xspacesolution@gmail.com>
2024-03-12 21:33:44 +0900

    feat: add redux feature on this project

---
+++ src/store/index.tsx
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import appSlice from "./appSlice";
+
+export const store = configureStore({
+  reducer: {
+    app: appSlice,
+  },
+});
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
